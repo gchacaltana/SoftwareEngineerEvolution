eda5a4e48bb5d749657672f4ab790b408319e1a0
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-09-27 00:50:25 -0500

    Create 10_tenth_year.py

---
+++ 10_tenth_year.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Programa que imprime en pantalla el mensaje Hola Mundo"""
+__author__ = 'Gonzalo Chacaltana'
+__email__ = "gchacaltanab@outlook.com"
+  
+class HelloWorld(object):
+    def __init__(self):
+        print ("Hola Mundo")
+  
+obj = HelloWorld()
